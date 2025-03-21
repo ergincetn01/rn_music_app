80784c7a8bbf677fe79c7b4665cb2ca759b3d777
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-22 01:05:33 +0300

    tabs layout, screens & icons, tabbar view modified

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,82 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from "expo-router"
+import React from "react"
+import { Colors } from "@/constants/Colors"
+import { Entypo, Feather } from "@expo/vector-icons"
+import { LinearGradient } from "expo-linear-gradient"
+import { StyleSheet, View } from "react-native"
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarActiveTintColor: Colors.tintColorActive,
+				headerShown: false,
+				tabBarInactiveTintColor: "#B0B0B0",
+				tabBarStyle: {
+					height: 60,
+					overflow: "hidden",
+					borderTopRightRadius: 16,
+					borderTopLeftRadius: 16,
+					position: "absolute",
+					borderTopWidth: 0,
+					paddingTop: 2,
+					borderBottomLeftRadius: 16,
+					borderBottomRightRadius: 16,
+					marginBottom: 8,
+					marginHorizontal: 6,
+				},
+				tabBarLabelStyle: {
+					marginTop: 4,
+					fontSize: 12,
+				},
+				animation: "fade",
+				tabBarBackground: () => (
+					<View
+						style={{
+							height: 100,
+							width: "100%",
+							backgroundColor: "black",
+						}}
+					>
+						<LinearGradient
+							colors={[Colors.granite, Colors.onyx]}
+							style={StyleSheet.absoluteFillObject}
+						/>
+					</View>
+				),
+			}}
+		>
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "All songs",
+					tabBarIcon: ({ color }) => (
+						<Feather name="music" color={color} size={28} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="playlists"
+				options={{
+					title: "Playlists",
+					tabBarIcon: ({ color }) => (
+						<Feather name="list" color={color} size={28} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="favorites"
+				options={{
+					title: "Favorites",
+					tabBarIcon: ({ color, focused }) => (
+						<Entypo
+							name={focused ? "heart" : "heart-outlined"}
+							color={color}
+							size={28}
+						/>
+					),
+				}}
+			/>
+		</Tabs>
+	)
 }
