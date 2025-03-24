ac1ede0344445767003ac4bc3a6d8c74f55005c7
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-24 22:07:48 +0300

    song images added and easy import constants impl

---
+++ constants/songImages.ts
@@ -0,0 +1,7 @@
+import toygar from "@/assets/images/songs/toygar.jpeg"
+import acitirgibi from "@/assets/images/songs/acitirgibi.jpeg"
+import baktigin from "@/assets/images/songs/baktigin.jpeg"
+import bensarkimi from "@/assets/images/songs/bensarkimi.jpeg"
+
+const songsImages = [bensarkimi, toygar, acitirgibi, baktigin]
+export default songsImages
